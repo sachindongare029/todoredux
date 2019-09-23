764fa409c742d214b572d0493bbfadca351e6e07
dongare sachin <sachindongare029@gmail.com>
2019-09-23 13:30:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Todos from './components/todos';
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+let input;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("submitted", input.value);
+    e.target.reset();
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="form-container">
+          <h3>TODO APP</h3>
+          <form onSubmit={e => this.handleSubmit(e)}>
+            <input
+              type="text"
+              placeholder="What do you want to do ?"
+              ref={node => (input = node)}
+            />
+            <button type="submit">Add </button>
+          </form>
+          <Todos />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
